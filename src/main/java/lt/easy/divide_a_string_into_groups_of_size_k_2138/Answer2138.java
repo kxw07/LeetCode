91c0128f797c26a48a9c74a01a99971e2ec8d87d
kai <kai@simbo.com.tw>
2022-10-10 16:19:24 +0800

    2138 improve

--- src/main/java/lt/easy/divide_a_string_into_groups_of_size_k_2138/Answer2138.java
+++ src/main/java/lt/easy/divide_a_string_into_groups_of_size_k_2138/Answer2138.java
@@ -1,34 +1,20 @@
 package lt.easy.divide_a_string_into_groups_of_size_k_2138;
 
 
-import java.util.ArrayList;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
-
 public class Answer2138 {
     public String[] divideString(String s, int k, char fill) {
-        char[] input = s.toCharArray();
-        ArrayList<String> result = new ArrayList<>();
-        StringBuilder tmp = new StringBuilder();
+        StringBuilder sb = new StringBuilder(s);
 
-        for (int idx = 0; idx < s.length(); idx++) {
-            if (k == tmp.length()) {
-                result.add(tmp.toString());
-                tmp = new StringBuilder();
-            }
-
-            tmp.append(input[idx]);
-
-            if (idx == s.length() - 1 && tmp.length() > 0) {
-                tmp.append(getFillChars(tmp.length(), k, fill));
-                result.add(tmp.toString());
-            }
+        while (sb.length() % k != 0) {
+            sb.append(fill);
         }
 
-        return result.toArray(new String[result.size()]);
-    }
+        String[] result = new String[sb.length() / k];
 
-    private String getFillChars(int left, int size, char fill) {
-        return IntStream.range(left, size).mapToObj(i -> Character.toString(fill)).collect(Collectors.joining());
+        for (int idx = 0; idx < sb.length() / k; idx++) {
+            result[idx] = sb.substring(idx * k, (idx + 1) * k);
+        }
+
+        return result;
     }
 }
