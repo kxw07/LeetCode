f5d08660d8dad6bb67f27b5036be8c9f75dc9ad2
kai <kai@simbo.com.tw>
2022-10-16 22:37:13 +0800

    1598 version 2

--- src/main/java/lt/easy/crawler_log_folder_1598/Answer1598.java
+++ src/main/java/lt/easy/crawler_log_folder_1598/Answer1598.java
@@ -2,35 +2,55 @@ package lt.easy.crawler_log_folder_1598;
 
 public class Answer1598 {
     public int minOperations(String[] logs) {
+//        DirNode root = new DirNode("root", null, null);
+//        DirNode now = root;
+//
+//        String back = "../";
+//        String stay = "./";
+//
+//        for (String operator : logs) {
+//            if (operator.equals(back)) {
+//                if (now.getPrev() != null) {
+//                    now = now.getPrev();
+//                }
+//            } else if (operator.equals(stay)) {
+//
+//            } else {
+//                DirNode newDir = new DirNode(operator.substring(0, operator.length() - 1), now, null);
+//                now.setNext(newDir);
+//                now = newDir;
+//            }
+//        }
+//
+//        int result = 0;
+//        while (now.prev != null) {
+//            now = now.getPrev();
+//            result++;
+//        }
+//
+//        return result;
 
-        DirNode root = new DirNode("root", null, null);
-        DirNode now = root;
+        return secondVersion(logs);
+    }
 
+    public int secondVersion(String[] logs) {
         String back = "../";
         String stay = "./";
 
+        int level = 0;
         for (String operator : logs) {
             if (operator.equals(back)) {
-                if (now.getPrev() != null) {
-                    now = now.getPrev();
-                }
+                if (level > 0) level--;
             } else if (operator.equals(stay)) {
 
             } else {
-                DirNode newDir = new DirNode(operator.substring(0, operator.length() - 1), now, null);
-                now.setNext(newDir);
-                now = newDir;
+                level++;
             }
         }
 
-        int result = 0;
-        while (now.prev != null) {
-            now = now.getPrev();
-            result++;
-        }
-
-        return result;
+        return level;
     }
+
 
     private static class DirNode {
         private String name;
