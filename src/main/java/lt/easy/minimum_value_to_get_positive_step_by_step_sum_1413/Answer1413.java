65095e793d3dff5537f7deebf6dbea733f346da1
kai <kai@simbo.com.tw>
2022-10-23 13:45:53 +0800

    1413 version 2

--- src/main/java/lt/easy/minimum_value_to_get_positive_step_by_step_sum_1413/Answer1413.java
+++ src/main/java/lt/easy/minimum_value_to_get_positive_step_by_step_sum_1413/Answer1413.java
@@ -1,27 +1,24 @@
 package lt.easy.minimum_value_to_get_positive_step_by_step_sum_1413;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class Answer1413 {
 
     public int minStartValue(int[] nums) {
-        List<Integer> sumArray = new ArrayList<>();
+        AtomicInteger minValue = new AtomicInteger();
+        AtomicInteger previousSum = new AtomicInteger();
 
-        Arrays.stream(nums).reduce(0, (sum, num) -> {
-            int newSum = sum + num;
-            sumArray.add(newSum);
-
-            return newSum;
+        Arrays.stream(nums).forEach(num -> {
+            previousSum.set(previousSum.get() + num);
+            if (previousSum.get() < minValue.get()) minValue.set(previousSum.get());
         });
 
-        int minValue = sumArray.stream().mapToInt(num -> num).min().orElseThrow();
 
-        if (minValue >= 1) {
+        if (minValue.get() >= 1) {
             return 1;
         } else {
-            return 1 - minValue;
+            return 1 - minValue.get();
         }
     }
 }
