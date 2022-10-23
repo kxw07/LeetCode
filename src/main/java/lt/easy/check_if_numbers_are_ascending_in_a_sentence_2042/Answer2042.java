7c80f20b8ca58aedfb1b5606115a5d29fe794343
kai <kai@simbo.com.tw>
2022-10-23 14:30:10 +0800

    2042 version 2

--- src/main/java/lt/easy/check_if_numbers_are_ascending_in_a_sentence_2042/Answer2042.java
+++ src/main/java/lt/easy/check_if_numbers_are_ascending_in_a_sentence_2042/Answer2042.java
@@ -1,27 +1,29 @@
 package lt.easy.check_if_numbers_are_ascending_in_a_sentence_2042;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
 
 public class Answer2042 {
     public boolean areNumbersAscending(String s) {
-        final int[] numArray = Arrays.stream(s.split(" ")).filter(word -> {
+        String[] words = s.split(" ");
+        int lastNum = 0;
+
+        for (String word : words) {
             try {
-                Integer.parseInt(word);
+                int num = Integer.parseInt(word);
+                if (num <= lastNum) {
+                    return false;
+                }
+
+                lastNum = num;
             } catch (NumberFormatException e) {
-                return false;
+                doNothing();
             }
 
-            return true;
-        }).mapToInt(Integer::parseInt).toArray();
-
-        for (int idx = 1; idx < numArray.length; idx++) {
-            if (numArray[idx] <= numArray[idx - 1]) {
-                return false;
-            }
         }
 
         return true;
+    }
+
+    private void doNothing() {
+
     }
 }
