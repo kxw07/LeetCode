49339002973f0f0ea736ee01945d5e6d0386cfd6
kai <kai@simbo.com.tw>
2022-11-19 21:42:56 +0800

    1200 version 2

--- src/main/java/lt/easy/minimum_absolute_difference_1200/Answer1200.java
+++ src/main/java/lt/easy/minimum_absolute_difference_1200/Answer1200.java
@@ -6,26 +6,22 @@ import java.util.List;
 
 public class Answer1200 {
     public List<List<Integer>> minimumAbsDifference(int[] arr) {
+        Arrays.sort(arr);
+
         int minDistance = Integer.MAX_VALUE;
         List<List<Integer>> result = new ArrayList<>();
 
-        final int[] sortedArr = Arrays.stream(arr).sorted().toArray();
-        for (int idx = 0; idx < sortedArr.length; idx++) {
+        for (int idx = 0; idx < arr.length; idx++) {
             int nextIdx = idx + 1;
-            if (nextIdx == sortedArr.length) break;
+            if (nextIdx == arr.length) break;
 
-            int distance = sortedArr[nextIdx] - sortedArr[idx];
+            int distance = arr[nextIdx] - arr[idx];
             if (distance < minDistance) {
                 minDistance = distance;
-            }
-        }
-
-        for (int idx = 0; idx < sortedArr.length; idx++) {
-            int nextIdx = idx + 1;
-            if (nextIdx == sortedArr.length) break;
-
-            if ((sortedArr[nextIdx] - sortedArr[idx]) == minDistance) {
-                result.add(List.of(sortedArr[idx], sortedArr[nextIdx]));
+                result.clear();
+                result.add(List.of(arr[idx], arr[nextIdx]));
+            } else if (distance == minDistance) {
+                result.add(List.of(arr[idx], arr[nextIdx]));
             }
         }
 
