814d82f97f11ee57493f128511532944b2cb5134
kai <kai@simbo.com.tw>
2022-12-31 16:03:39 +0800

    206 version 2

--- src/main/java/lt/easy/reverse_linked_list_206/Answer206.java
+++ src/main/java/lt/easy/reverse_linked_list_206/Answer206.java
@@ -1,44 +1,18 @@
 package lt.easy.reverse_linked_list_206;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Answer206 {
     public ListNode reverseList(ListNode head) {
-        try {
-            if (head.next == null) return head;
-        } catch (NullPointerException e) {
-            return head;
+        ListNode current = head;
+        ListNode previous = null;
+
+        while (current != null) {
+            ListNode next = current.next;
+            current.next = previous;
+            previous = current;
+            current = next;
         }
 
-        List<ListNode> nodeList = new ArrayList<>();
-        getNodeReversedList(head, nodeList);
-
-        for (int idx = 0; idx < nodeList.size(); idx++) {
-            if (idx + 1 == nodeList.size()) {
-                nodeList.get(idx).next = null;
-                break;
-            }
-
-            nodeList.get(idx).next = nodeList.get(idx + 1);
-        }
-
-        return nodeList.get(0);
-    }
-
-    public void getNodeReversedList(ListNode node, List<ListNode> list) {
-        try {
-            if (node.next == null) {
-                list.add(node);
-                return;
-            }
-        } catch (NullPointerException e) {
-            list.add(node);
-            return;
-        }
-
-        getNodeReversedList(node.next, list);
-        list.add(node);
+        return previous;
     }
 
     static class ListNode {
