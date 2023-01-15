4b0ac9fcfb358aff4905f92ed19549adf0c621c6
kai <kai@simbo.com.tw>
2023-01-15 11:36:38 +0800

    1961 version 2

--- src/main/java/lt/easy/check_if_string_is_a_prefix_of_array_1961/Answer1961.java
+++ src/main/java/lt/easy/check_if_string_is_a_prefix_of_array_1961/Answer1961.java
@@ -3,20 +3,13 @@ package lt.easy.check_if_string_is_a_prefix_of_array_1961;
 
 public class Answer1961 {
     public boolean isPrefixString(String s, String[] words) {
-        if (s.length() > String.join("", words).length()) return false;
-
+        StringBuilder concat = new StringBuilder();
         for (String word : words) {
-            if (s.length() == 0) {
-                return true;
-            }
-
-            if (s.startsWith(word)) {
-                s = s.substring(word.length());
-            } else {
-                return false;
-            }
+            concat.append(word);
+            if (s.equals(concat.toString())) return true;
+            if (!s.startsWith(concat.toString())) return false;
         }
 
-        return true;
+        return false;
     }
 }
