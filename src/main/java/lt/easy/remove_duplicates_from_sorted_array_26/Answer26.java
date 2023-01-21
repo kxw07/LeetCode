108d06551350c7a9676414013e5cac38047d04e6
kai <kai@simbo.com.tw>
2023-01-21 20:10:09 +0800

    26 version 2

--- src/main/java/lt/easy/remove_duplicates_from_sorted_array_26/Answer26.java
+++ src/main/java/lt/easy/remove_duplicates_from_sorted_array_26/Answer26.java
@@ -1,28 +1,19 @@
 package lt.easy.remove_duplicates_from_sorted_array_26;
 
 
+import java.util.Arrays;
+
 public class Answer26 {
     public int removeDuplicates(int[] nums) {
-        int lastIndex = nums.length;
+        int nonDuplicatedNumIndex = 1;
 
-        for (int index = 0; index < lastIndex; index++) {
-            if (index == 0) {
-                continue;
-            }
-
-            if (nums[index] == nums[index - 1]) {
-                int tmp = nums[index];
-                for (int idx = index; idx < nums.length; idx++) {
-                    if (idx + 1 == nums.length) continue;
-
-                    nums[idx] = nums[idx + 1];
-                }
-                nums[nums.length - 1] = tmp;
-                lastIndex--;
-                index--;
+        for (int idx = 0; idx < nums.length - 1; idx++) {
+            if (nums[idx] < nums[idx + 1]) {
+                nums[nonDuplicatedNumIndex] = nums[idx + 1];
+                nonDuplicatedNumIndex++;
             }
         }
 
-        return lastIndex;
+        return nonDuplicatedNumIndex;
     }
 }
