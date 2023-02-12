b0e024a442bac13b4e352735831536b55a051d92
kai <kai@simbo.com.tw>
2023-02-12 18:11:01 +0800

    303 version 2

--- src/main/java/lt/easy/range_sum_query_immutable_303/NumArray.java
+++ src/main/java/lt/easy/range_sum_query_immutable_303/NumArray.java
@@ -1,16 +1,22 @@
 package lt.easy.range_sum_query_immutable_303;
 
-import java.util.Arrays;
-
 public class NumArray {
 
+    private final int[] prefixSum;
     private final int[] nums;
 
     public NumArray(int[] nums) {
         this.nums = nums;
-    }
+        prefixSum = new int[nums.length];
+        prefixSum[0] = nums[0];
 
+        int sum = prefixSum[0];
+        for (int idx = 1; idx < prefixSum.length; idx++) {
+            sum += nums[idx];
+            prefixSum[idx] = sum;
+        }
+    }
     public int sumRange(int left, int right) {
-        return Arrays.stream(Arrays.copyOfRange(nums, left, right + 1)).sum();
+        return prefixSum[right] - prefixSum[left] + nums[left];
     }
 }
